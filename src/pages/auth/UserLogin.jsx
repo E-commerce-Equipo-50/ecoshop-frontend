cefc092bd4546c3c2a1e0ce70c2ca77a8134156e
SCIANMAA <Sciammarellaagus9@gmail.com>
2025-12-01 18:30:20 -0300

    Realizar cambios solicitados

--- src/pages/auth/UserLogin.jsx
+++ src/pages/auth/UserLogin.jsx
@@ -1,14 +1,13 @@
-export function AdminLogin() { 
+const UserLogin = () => {
   return (
     <>
-        {/* CONTENEDOR PRINCIPAL (Fondo Claro)
+      {/* CONTENEDOR PRINCIPAL (Fondo Claro)
           - min-h-screen: Ocupa toda la altura de la pantalla
           - bg-gray-50: Color de fondo gris muy claro (casi blanco)
           - flex ... center: Centra el contenido vertical y horizontalmente
         */}
 
       <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50">
-        
         {/* TÍTULO (Fuera de la tarjeta) */}
         <div className="sm:mx-auto sm:w-full sm:max-w-md mb-6">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -24,34 +23,34 @@ export function AdminLogin() {
 
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-gray-900 py-8 px-4 shadow-2xl sm:rounded-xl sm:px-10 border border-gray-700">
-            
             {/* LOGO */}
             <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-6">
               <img
                 alt="EcoShop Logo"
                 src="src/assets/home.png"
-                className="mx-auto h-16 w-auto" 
+                className="mx-auto h-16 w-auto"
               />
             </div>
 
-
             {/* FORMULARIO */}
             <form action="#" method="POST" className="space-y-6">
-              
               {/* CAMPO: EMAIL */}
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-200">
-                  Correo de la organización
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-200"
+                >
+                  Correo personal
                 </label>
                 <div className="mt-2">
                   <input
                     id="email"
                     name="email"
                     type="email"
-                    placeholder="Email"
+                    placeholder="jhondoe@email.com"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6 pl-2"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-[--primary-medium] sm:text-sm sm:leading-6 pl-2"
                   />
                 </div>
               </div>
@@ -59,7 +58,10 @@ export function AdminLogin() {
               {/* CAMPO: CONTRASEÑA */}
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-200">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-200"
+                  >
                     Contraseña
                   </label>
                 </div>
@@ -68,10 +70,10 @@ export function AdminLogin() {
                     id="password"
                     name="password"
                     type="password"
-                    placeholder="Contraseña"
+                    placeholder="..............."
                     required
                     autoComplete="new-password"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6 pl-2"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-[--primary-medium] sm:text-sm sm:leading-6 pl-2"
                   />
                 </div>
               </div>
@@ -80,24 +82,28 @@ export function AdminLogin() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-[--primary-medium] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[--primary-light] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 transition-colors"
+                  className="flex w-full justify-center rounded-md bg-[--primary-medium] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[--primary-light] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[--primary-medium] transition-colors"
                 >
                   Iniciar sesión
                 </button>
               </div>
             </form>
 
-            {/* LINK DE INICIO DE SESIÓN */}
+            {/* LINK DE CREAR CUENTA */}
             <p className="mt-10 text-center text-sm text-gray-400">
-              ¿No tienes cuenta?{' '}
-              <a href="/login" className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors">
-                Regístrate {/* El link debe llevarte al AdminRegister */}
+              ¿No tienes cuenta?{" "}
+              <a
+                href="/login"
+                className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors"
+              >
+                Regístrate {/* El link debe llevarte al UserRegister */}
               </a>
             </p>
-
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+export default UserLogin;
--- src/pages/auth/UserRegister.jsx
+++ src/pages/auth/UserRegister.jsx
@@ -1,13 +1,12 @@
-export function UserRegister() { 
+const UserRegister = () => {
   return (
     <>
-        {/* CONTENEDOR PRINCIPAL (Fondo Claro)
+      {/* CONTENEDOR PRINCIPAL (Fondo Claro)
           - min-h-screen: Ocupa toda la altura de la pantalla
           - bg-gray-50: Color de fondo gris muy claro (casi blanco)
           - flex ... center: Centra el contenido vertical y horizontalmente
       */}
       <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50">
-        
         {/* TÍTULO (Fuera de la tarjeta) */}
         <div className="sm:mx-auto sm:w-full sm:max-w-md mb-6">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -23,23 +22,23 @@ export function UserRegister() {
 
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-gray-900 py-8 px-4 shadow-2xl sm:rounded-xl sm:px-10 border border-gray-700">
-            
             {/* LOGO */}
             <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-6">
               <img
                 alt="EcoShop Logo"
                 src="src/assets/home.png"
-                className="mx-auto h-16 w-auto" 
+                className="mx-auto h-16 w-auto"
               />
             </div>
 
-
             {/* FORMULARIO */}
             <form action="#" method="POST" className="space-y-6">
-              
               {/* CAMPO: NOMBRE */}
               <div>
-                <label htmlFor="Name" className="block text-sm font-medium text-gray-200">
+                <label
+                  htmlFor="Name"
+                  className="block text-sm font-medium text-gray-200"
+                >
                   Nombre completo
                 </label>
                 <div className="mt-2">
@@ -47,28 +46,31 @@ export function UserRegister() {
                     id="Name"
                     name="Name"
                     type="text"
-                    placeholder="Nombre y apellido"
+                    placeholder="John Doe"
                     required
                     autoComplete="name"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6 pl-2"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-[--primary-medium] sm:text-sm sm:leading-6 pl-2"
                   />
                 </div>
               </div>
-              
+
               {/* CAMPO: EMAIL */}
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-200">
-                  Correo 
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-200"
+                >
+                  Correo
                 </label>
                 <div className="mt-2">
                   <input
                     id="email"
                     name="email"
                     type="email"
-                    placeholder="Email"
+                    placeholder="johndoe@email.com"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6 pl-2"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-[--primary-medium] sm:text-sm sm:leading-6 pl-2"
                   />
                 </div>
               </div>
@@ -76,7 +78,10 @@ export function UserRegister() {
               {/* CAMPO: CONTRASEÑA */}
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-200">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-200"
+                  >
                     Contraseña
                   </label>
                 </div>
@@ -85,10 +90,10 @@ export function UserRegister() {
                     id="password"
                     name="password"
                     type="password"
-                    placeholder="Contraseña"
+                    placeholder="..............."
                     required
                     autoComplete="new-password"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6 pl-2"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-[--primary-medium] sm:text-sm sm:leading-6 pl-2"
                   />
                 </div>
               </div>
@@ -97,24 +102,28 @@ export function UserRegister() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-[--primary-medium] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[--primary-light] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 transition-colors"
+                  className="flex w-full justify-center rounded-md bg-[--primary-medium] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[--primary-light] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[--primary-medium] transition-colors"
                 >
                   Registrarse
                 </button>
               </div>
             </form>
 
-              {/* LINK DE INICIO DE SESIÓN */}
+            {/* LINK DE INICIO DE SESIÓN */}
             <p className="mt-10 text-center text-sm text-gray-400">
-              ¿Ya tienes cuenta?{' '}
-              <a href="/login" className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors">
+              ¿Ya tienes cuenta?{" "}
+              <a
+                href="/login"
+                className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors"
+              >
                 Inicia sesión {/* El link debe llevarte al UserLogin */}
               </a>
             </p>
-
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+export default UserRegister;
