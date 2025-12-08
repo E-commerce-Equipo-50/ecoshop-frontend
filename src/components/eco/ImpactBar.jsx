f24b9e7cab91c463df0e5419d8c19d0d4064c106
Araf05 <aracelifernandez.3006@gmail.com>
2025-12-08 15:36:51 -0300

    Crear componente ImpactBar

--- src/components/eco/ImpactBar.jsx
+++ src/components/eco/ImpactBar.jsx
@@ -1,7 +1,26 @@
-export const ImpactBar = () => {
+export default function ImpactBar({ ecoScore }) {
+   if (!ecoScore) return null;
+  
+  // Buscar CO2
+  const co2Metric = ecoScore.metrics.find(m => m.type === "CO2");
+
+  //Si el producto no contiene metricas
+  if (!co2Metric) {
+    return (
+      <div className="p-3 bg-transparent rounded-md text-[var(--text-dark)] text-sm">
+        <p>Sin métricas de CO₂ disponibles.</p>
+      </div>
+    );
+  }
+
+  //Si tiene metricas, se muestra el CO2 evitado
+  const { value, comparisonValue } = co2Metric;
+
   return (
-    <div className='ImpactBar'>
-      ImpactBar Component
+    <div>
+      <p className="text-[var(--text-dark)] text-sm">
+        ¡Tu compra evita ~{comparisonValue-value}kg de CO2!
+      </p>
     </div>
   )
 }
