f1bcfc37c6080991289bcdbbb38097e933cff6f9
Daniel Morillo <dev-07@mennobot.com>
2025-12-09 10:35:52 -0500

    feat(App.jsx): importar las rutas y sus componentes para ser renderizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/layout/Navbar"
 import { Home } from "./pages/Home"
+import { Catalog } from "./pages/Catalog"
+import { Certifications } from "./pages/Certifications"
+import { Brands } from "./pages/Brands"
+import UserLogin from "./pages/auth/UserLogin"
+import UserRegister from "./pages/auth/UserRegister"
+import AdminLogin from "./pages/auth/AdminLogin"
+import AdminRegister from "./pages/auth/AdminRegister"
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/certifications" element={<Certifications />} />
+
+        <Route path="/login-client" element={<UserLogin />} />
+        <Route path="/register-client" element={<UserRegister />} />
+        <Route path="/login-company" element={<AdminLogin />} />
+        <Route path="/register-company" element={<AdminRegister />} />
+      </Routes>
     </>
   )
 }
