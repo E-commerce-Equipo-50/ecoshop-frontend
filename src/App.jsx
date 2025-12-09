e952bf90285100f860eec12aeb0577e8632fa67f
Daniel Morillo <dev-07@mennobot.com>
2025-12-09 09:55:52 -0500

    refactor(App.jsx): limpiar contenido temporal para integración de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import { TestComponent } from './store/TestComponent'
-
 export const App = () => {
   return (
-    <div className='App'>
-      <b> App Component </b>
-       <h1 className="text-green-500 text-4xl">Tailwind v3 funciona 💚</h1>
-      <p>
-        En este componente se incluirán todas las páginas de /src/pages para hacer la mejor app frontend de la historia
-      </p>
-      <TestComponent />
-    </div>
+    <>
+    </>
   )
 }
