8d670fc0355fd1a9aab518773e837299cc97e1e2
Daniel Morillo <dev-07@mennobot.com>
2025-12-09 09:57:58 -0500

    feat(App.jsx): importar componentes Navbar y Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/layout/Navbar"
+import { Home } from "./pages/Home"
+
 export const App = () => {
   return (
     <>
+      <Navbar />
+      <Home />
     </>
   )
 }
