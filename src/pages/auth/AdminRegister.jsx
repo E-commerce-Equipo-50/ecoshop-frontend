99b1f4bf51c338543477cc60bea2e1bf9e9dad49
Daniel Morillo <dev-07@mennobot.com>
2025-12-11 17:03:04 -0500

    fix(auth): redirigir correctamente entre login y register

--- src/pages/auth/AdminRegister.jsx
+++ src/pages/auth/AdminRegister.jsx
@@ -167,7 +167,7 @@ const AdminRegister = () => {
             <p className="mt-10 text-center text-sm text-gray-400">
               ¿Ya tienes cuenta?{" "}
               <a
-                href="/login"
+                href="/login-company"
                 className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors"
               >
                 Inicia sesión {/* El link debe llevarte al AdminLogin */}
--- src/pages/auth/UserLogin.jsx
+++ src/pages/auth/UserLogin.jsx
@@ -144,7 +144,7 @@ const UserLogin = () => {
             <p className="mt-10 text-center text-sm text-gray-400">
               ¿No tienes cuenta?{" "}
               <a
-                href="/login"
+                href="/register-client"
                 className="font-semibold leading-6 text-[--primary-medium] hover:text-[--primary-light] transition-colors"
               >
                 Regístrate {/* El link debe llevarte al UserRegister */}
